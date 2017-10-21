d5aabc832c530fd19dca76a93a76a0a0bafa8522
tmac4403 <drive911@gmail.com>
2017-10-21 13:58:21 -0500

    2

--- nyt.js
+++ nyt.js
@@ -1,12 +1,12 @@
  
 
- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-    <script type="text/javascript">;
+ // <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+ //    <script type="text/javascript">;
       // This .on("click") function will trigger the AJAX Call
 
 
 
-      $("#find-news").on("click", function(event) {
+      $("#news1").on("click", function(event) {
 
         // Preventing the submit button from trying to submit the form
         // We're optionally using a form so the user may hit Enter to search instead of clicking the button
@@ -14,10 +14,9 @@
         // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
         // Here we grab the text from the input box
-        var news = $("#news-input").val();
+        // var news = $("#news-input").val();
 
-        // Here we construct our URL
-        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
+      
 
         // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
         // and display it in the div with an id of movie-view
@@ -32,10 +31,11 @@ var url = "https://api.nytimes.com/svc/archive/v1/2016/1.json";
     }).done(function(response) {
         console.log(queryURL);
     console.log(response);
+  });
       // Create CODE HERE to transfer content to HTML$()
-$("#movie-view").html("<h1>" + JSON.stringify(response) + "news Details</h1>");// new from Code School
+$("#news-view").html("<h1>" + JSON.stringify(response) + "news Details</h1>");// new from Code School
 
-
+});
 
 
 
